22a3d72f1325bcd3504643900d42a9220cfbffb5
JustBraf <dwaynecorey+github@gmail.com>
2019-03-21 16:26:49 -0400

    added template, insert and view of images

--- client/main.js
+++ client/main.js
@@ -2,21 +2,43 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../lib/collection.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.myJumbo.events({
+	'click .js-addImg'(event){
+		$("#addImgModal").modal("show");
+	}
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.addImg.events({
+	'click .js-saveImg'(event){
+		var imgTitle = $("#imgTitle").val();
+		var imgPath = $("#imgPath").val();
+		var imgDesc = $("#imgDesc").val();
+		
+		$("#imgTitle").val('');
+		$("#imgPath").val('');
+		$("#imgDesc").val('');
+		$("#addImgModal").modal("hide");
+		imagesDB.insert({"title":imgTitle, "path":imgPath, "desc":imgDesc, "createdOn":Date()});
+	},
+	'click .js-cancelAdd'(){
+		$("#imgTitle").val('');
+		$("#imgPath").val('');
+		$("#imgDesc").val('');
+		$("#addImgModal").modal("hide");
+	},
+	'input #imgPath'(event){
+		var imgPath = $("#imgPath").val();
+		$("#addImgPreview").attr('src', imgPath);
+	}
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.mainBody.helpers({
+	imagesFound(){
+		return imagesDB.find().count();
+	},
+	allImages(){
+		return imagesDB.find();
+	}
 });
