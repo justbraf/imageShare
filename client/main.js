f70e47ed251f860ee3b82eed0cd0ba3ca4514fa3
justbraf <dwaynecorey+github@gmail.com>
2019-04-11 01:36:56 -0400

    updating meteor core and adding accounts package

--- client/main.js
+++ client/main.js
@@ -4,6 +4,21 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import '../lib/collection.js';
 
+lastScrollTop = 0; 
+$(window).scroll(function(event){
+	// test if we are near the bottom of the window
+	if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+		// where are we in the page? 
+		var scrollTop = $(this).scrollTop();
+		// test if we are going down
+		if (scrollTop > lastScrollTop){
+			// yes we are heading down...   
+		 console.log("We have arrived at the bottom of the page");
+		}
+		lastScrollTop = scrollTop;
+	}
+});
+
 Template.myJumbo.events({
 	'click .js-addImg'(event){
 		$("#addImgModal").modal("show");
